ff29594b5d845066bad8c5264ae549599a24b2b2
Dmitry Moskowski <me@corpix.ru>
2017-11-28 19:21:49 +0000

    Reverse close order

--- closer/closers.go
+++ closer/closers.go
@@ -8,14 +8,16 @@ type Closers []io.Closer
 
 func (c Closers) Close() error {
 	var (
+		n   = len(c) - 1
 		err error
 	)
 
-	for _, closer := range c {
-		err = closer.Close()
+	for n >= 0 {
+		err = c[n].Close()
 		if err != nil {
 			return err
 		}
+		n--
 	}
 
 	return nil
